74fe06343d1e6b9100f7ee50055cf0c8486429be
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2019-01-16 13:00:26 +0530

    implemented request handling in app
    implemented request handling for catalogue page in app
    can handle onli limited requests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+	let path = './index.html';
+	if (req.url == '/styleSheet.css') {
+		path = './styleSheet.css';
+	}
+	if (req.url == '/freshorigins.jpg') {
+		path = './freshorigins.jpg';
+	}
+	fs.readFile(path, (err, content) => {
+		res.write(content);
+		res.end();
+	});
 };
 
 // Export a function that can act as a handler
