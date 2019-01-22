507871a3f30c04cb4cd68eea4918fa4129497e54
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2019-01-22 18:15:06 +0530

    rewrote the app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 const fs = require('fs');
-const app = (req, res) => {
-	let path = './html-data' + req.url;
-	if (path == './html-data/') {
-		path = './html-data/index.html';
+const RequestHandler = require('./requestHandler');
+const app = new RequestHandler();
+
+const logRequests = function(req, res, next) {
+	console.log(req.url);
+	console.log(req.method);
+	console.log('Headers-->/n', req.headers);
+	next();
+};
+
+const addPathPrefix = url => `./public${url}`;
+
+const getPath = function(url) {
+	let path = addPathPrefix(url);
+	if (url === '/') {
+		path = addPathPrefix('/index.html');
 	}
+	return path;
+};
+
+const serveFile = function(req, res) {
+	let path = getPath(req.url);
 	fs.readFile(path, (err, content) => {
 		if (!err) {
 			res.statusCode = 200;
@@ -16,6 +33,9 @@ const app = (req, res) => {
 	});
 };
 
+app.use(logRequests);
+app.use(serveFile);
+
 // Export a function that can act as a handler
 
-module.exports = app;
+module.exports = app.handleRequest.bind(app);
